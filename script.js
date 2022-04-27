fbe70f6ef089c498a9148b6844f4c276a2fafedc
raed ABBAS <raed.abbas@hotmail.fr>
2022-04-28 01:45:38 +0200

    fin

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let claNote = document.getElementsByName("option");
-a = document.getElementById("note");
-let form= document.createElement('form');
-let label =document.createElement('label');
-label.textContent="Liste des notes classiques";
-form.append(label);
-let select=document.createElement('select');
-// tableau des notes classiques
-let amNotes = ["", "do", "re", "mi", "fa", "sol", "la", "si"];
 
-for (i = 0; i < amNotes.length; i++) {
-    option = document.createElement("option");
-    option.id = amNotes[i];
-    option.innerText = amNotes[i];
-    select.appendChild(option);
-}
-form.appendChild(select);
-document.getElementById('main').appendChild(form);
-//select.onselect
+
+// tableau des notes classiques
+
+const notAmerique = ['', 'c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+
+notAmerique.forEach(element => {
+    let option = document.createElement('option');
+    option.innerHTML = element;
+    document.getElementById('notesAmeriques').appendChild(option);
+});
+let selectNC = document.getElementById('notesClassiques');
+selectNC.addEventListener('change', function () {
+    for (var i = 0; i < selectNC.length; i++) {
+        if (selectNC[i].selected == true) {
+
+            let selectNA = document.getElementById('notesAmeriques');
+            selectNA[i].selected = true;
+
+            let res = document.getElementById('res');
+            res.textContent = 'La notation americaine pour ' + selectNC[i].value + ' est ' + selectNA[i].value;
+        }
+
+    }
+
+})
